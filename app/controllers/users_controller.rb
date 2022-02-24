40adeb844dba8b5328edfcb014dc3a13409696c2
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-24 09:59:08 -0600

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def show 
+        user = User.find(params[:id])
+        if user
+            render json: user, include: :items 
+        else
+            render json: {error: "User Not Found"}, status: :not_found
+        end 
+    end
 end
